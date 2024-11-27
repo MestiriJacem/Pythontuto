78372d11c116256ee992036b1f8d41eb468dfc5e
Jacem mestiri <mestirijacemm@gmail.com>
2024-11-27 09:04:55 +0100

    adding a variable to a text using the f

--- Variables.py
+++ Variables.py
@@ -1,6 +1,7 @@
 #Variables(string , integer, boolean , float)
 first_name ="fatma"
-
+email ="fatma@gmail.com"
 print (first_name)
 #adding a variable to a string
 print( f"hello {first_name}")
+print (f"Your email is {email}")
