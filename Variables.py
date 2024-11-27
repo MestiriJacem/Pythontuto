33dc47339228656380080319e44f4cd46277a29e
Jacem mestiri <mestirijacemm@gmail.com>
2024-11-27 09:06:37 +0100

    Integers

--- Variables.py
+++ Variables.py
@@ -1,7 +1,11 @@
 #Variables(string , integer, boolean , float)
+#1-Strings
 first_name ="fatma"
 email ="fatma@gmail.com"
 print (first_name)
 #adding a variable to a string
 print( f"hello {first_name}")
 print (f"Your email is {email}")
+
+#-2Integers
+age =25
